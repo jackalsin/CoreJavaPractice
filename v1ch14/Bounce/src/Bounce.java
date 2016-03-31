a77c5a4e3a6a0f2cd6cb1dfef554a63cdcc820cb
jackalsin <jackal.sin@hotmail.com>
2016-03-31 02:19:17 -0400

    threadPoolUpdate

--- v1ch14/Bounce/src/Bounce.java
+++ v1ch14/Bounce/src/Bounce.java
@@ -4,98 +4,96 @@ import javax.swing.*;
 
 /**
  * Shows an animated bouncing ball.
- * @version 1.33 2007-05-17
+ *
  * @author Cay Horstmann
+ * @version 1.33 2007-05-17
  */
-public class Bounce
-{
-   public static void main(String[] args)
-   {
-      EventQueue.invokeLater(new Runnable()
-         {
-            public void run()
-            {
-               JFrame frame = new BounceFrame();
-               frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-               frame.setVisible(true);
+public class Bounce {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new BounceFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
             }
-         });
-   }
+        });
+    }
 }
 
 /**
  * The frame with ball component and buttons.
  */
-class BounceFrame extends JFrame
-{
-   /**
-    * Constructs the frame with the component for showing the bouncing ball and Start and Close
-    * buttons
-    */
-   public BounceFrame()
-   {
-      setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-      setTitle("Bounce");
+class BounceFrame extends JFrame {
+    /**
+     * Constructs the frame with the component for showing the bouncing ball and Start and Close
+     * buttons
+     */
+    public BounceFrame() {
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        setTitle("Bounce");
 
-      comp = new BallComponent();
-      add(comp, BorderLayout.CENTER);
-      JPanel buttonPanel = new JPanel();
-      addButton(buttonPanel, "Start", new ActionListener()
-         {
-            public void actionPerformed(ActionEvent event)
-            {
-               addBall();
+        comp = new BallComponent();
+        add(comp, BorderLayout.CENTER);
+        JPanel buttonPanel = new JPanel();
+        addButton(buttonPanel, "Start", event -> {
+            addBall();
+        });
+
+        addButton(buttonPanel, "Close", new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                System.exit(0);
             }
-         });
+        });
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
 
-      addButton(buttonPanel, "Close", new ActionListener()
-         {
-            public void actionPerformed(ActionEvent event)
-            {
-               System.exit(0);
+    /**
+     * Adds a button to a container.
+     *
+     * @param c        the container
+     * @param title    the button title
+     * @param listener the action listener for the button
+     */
+    public void addButton(Container c, String title, ActionListener listener) {
+        JButton button = new JButton(title);
+        c.add(button);
+        button.addActionListener(listener);
+    }
+
+    /**
+     * Adds a bouncing ball to the panel and makes it bounce 1,000 times.
+     */
+    public void addBall() {
+        Ball ball = new Ball();
+        comp.add(ball);
+
+        Thread ballMoveThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= STEPS; i++) {
+                    ball.move(comp.getBounds());
+                    comp.paint(comp.getGraphics());
+                    System.out.println("Hit once" + count++);
+                    try {
+                        Thread.sleep(DELAY);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } // end of for loop
             }
-         });
-      add(buttonPanel, BorderLayout.SOUTH);
-   }
+        });
+        ballMoveThread.start();
 
-   /**
-    * Adds a button to a container.
-    * @param c the container
-    * @param title the button title
-    * @param listener the action listener for the button
-    */
-   public void addButton(Container c, String title, ActionListener listener)
-   {
-      JButton button = new JButton(title);
-      c.add(button);
-      button.addActionListener(listener);
-   }
+    }
 
-   /**
-    * Adds a bouncing ball to the panel and makes it bounce 1,000 times.
-    */
-   public void addBall()
-   {
-      try
-      {
-         Ball ball = new Ball();
-         comp.add(ball);
+    
 
-         for (int i = 1; i <= STEPS; i++)
-         {
-            ball.move(comp.getBounds());
-            comp.paint(comp.getGraphics());
-            Thread.sleep(DELAY);
-         }
-      }
-      catch (InterruptedException e)
-      {
-      }
-   }
 
-   private BallComponent comp;
-   public static final int DEFAULT_WIDTH = 450;
-   public static final int DEFAULT_HEIGHT = 350;
-   public static final int STEPS = 1000;
-   public static final int DELAY = 3;
+
+    private BallComponent comp;
+    public static final int DEFAULT_WIDTH = 450;
+    public static final int DEFAULT_HEIGHT = 350;
+    public static final int STEPS = 10000;
+    public static final int DELAY = 300; // the unit is millisecond
+    private static int count = 0;
 }
